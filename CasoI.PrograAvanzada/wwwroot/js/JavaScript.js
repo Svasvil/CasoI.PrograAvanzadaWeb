ebd080ce265dadbc8dda70c1fed12b43ed4d2f05
Sebastian Vasquez <svasvil0211@gmail.com>
2026-03-10 10:31:49 -0600

    ultimos cambios , y creacion de vistas

---
+++ CasoI.PrograAvanzada/wwwroot/js/JavaScript.js
@@ -0,0 +1,20 @@
+﻿<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const form = document.getElementById('createTaskForm');
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+    Swal.fire({
+        title: 'crear tarea?',
+    text: 'seguro de que deseas crear esta tarea?',
+    icon: 'question',
+    showCancelButton: true,
+    confirmButtonText: 'Si',
+    cancelButtonText: 'Cancelar'
+            }).then((result) => {
+                if (result.isConfirmed) {
+        form.submit();
+                }
+            });
+        });
+    });
+</script>
